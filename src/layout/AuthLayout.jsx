df917f5baf41e648413e6580941f25f9dd3af213
Khubaib Farooq <khubaibfarooq@evampsaanga.com>
2024-05-06 10:39:15 +0500

    update the login screen

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 // @ import media
 import boy from "../assets/image/boy.png";
+import loginImg from "../assets/image/login-img.png"
 import LogoImage from "../assets/image/grazleLogo.png";
 // @ import styles
 import "./index.css";
@@ -12,14 +13,12 @@ const AuthLayout = () => {
       <div className="container">
         <div className="row justify-content-center align-items-center">
           <div className="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
-            <div className="side-textbox">
-              <p>Discover endless possibilities</p>
-              <h3>Explore, buy, and sell with our vibrant marketplace!"</h3>
-              <img src={boy} alt="boy" />
+            <div className="side-textbox d-flex justify-content-end">
+              <img src={loginImg} />
             </div>
           </div>
           <div className="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
-            <div className="text-center">
+            <div className="text-center loginImg">
               <img src={LogoImage} alt="LogoImage" />
               <Outlet />
             </div>
